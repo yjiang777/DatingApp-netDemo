9951e1ec6f28c96b36867f3d4a33273cae0cd85c
yjiang479 <yjiang479@gatech.edu>
2025-06-18 21:13:17 -0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,11 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 
