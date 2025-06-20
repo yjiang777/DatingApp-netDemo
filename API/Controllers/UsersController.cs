64dd49d2f8943113f255aa29ea0646e1196eb0dc
yjiang479 <yjiang479@gatech.edu>
2025-06-19 21:42:15 -0700

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,13 +3,18 @@ using API.Entities;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
-namespace API;
+namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")] // /api/users
 
-public class UsersController(DataContext context) : ControllerBase {
+[Authorize]
+public class UsersController(DataContext context) : BaseApiController
+{
+
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -17,7 +22,7 @@ public class UsersController(DataContext context) : ControllerBase {
 
         return users;
     }
-
+    [Authorize]
     [HttpGet("{id}")] // /api/users/2
     public async Task<ActionResult<AppUser>> GetUsers(int id)
     {
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,10 @@ namespace API.Entities;
 public class AppUser
 {
     public int Id { get; set; }
-    public String UserName { get; set; } = "";
+    public required String UserName { get; set; }
+
+    public required byte[] PasswordHash { get; set; }
+
+    public required byte[] PasswordSalt { get; set; }
 
 }
